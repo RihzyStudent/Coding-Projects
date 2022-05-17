5c184604c679a49dc87a1fe26cd1d0a146703149
Rihzy <dhruv.behl.1604@podarinternationalschool.com>
2022-05-17 10:00:18 +0530

    New proj

---
+++ Coding Projects/Section 12/Linked Chain Fun/Node.h
@@ -0,0 +1,34 @@
+#ifndef LINKED_CHAIN_FUN_NODE_H
+#define LINKED_CHAIN_FUN_NODE_H
+
+class Node
+        {
+    public:
+        Node(int data, Node* next)
+        {
+            this->data=data;
+            this->next=next;
+        }
+        void setData(int data)
+        {
+            this->data=data;
+        }
+        void setNext(Node* next)
+        {
+            this->next=next;
+        }
+        int getData() const
+        {
+            return data;
+        }
+        Node* getNext() const
+        {
+            return next;
+        }
+
+    private:
+        int data;
+        Node* next;
+};
+
+#endif //LINKED_CHAIN_FUN_NODE_H
