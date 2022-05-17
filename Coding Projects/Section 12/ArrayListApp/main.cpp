7349d4e03710fd42510be9cbd1323f434830ddea
Rihzy <dhruv.behl.1604@podarinternationalschool.com>
2022-05-17 13:06:39 +0530

    Section 12
    Signed-off-by: Rihzy <dhruv.behl.1604@podarinternationalschool.com>

--- Coding Projects/Section 12/ArrayListApp/main.cpp
+++ Coding Projects/Section 12/ArrayListApp/main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
-
+#include <stdexcept>
+#include "ArrayList.h"
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    ArrayList myList;
+
+    for (int i = 0; i < 15; ++i) {
+        myList.add(i*10);
+    }
+
+    myList.printList();
+    cout<<endl<<endl;
+    cout<<"size is: "<<myList.size()<<endl;
+
+    myList.add(555,15);
+    myList.printList();
+    cout<<"Size is now: "<<myList.size()<<endl;
+
+    myList.set(987,3);
+    try {
+        myList.add(900);
+    }
+    catch (exception& err) {
+        cout<<err.what();
+    }
+    
+    myList.printList();
+
     return 0;
 }
